cd1abf85ab89e29e8b1d898d1eec8e30d13bb177
Shaik Basha Vali <bashavali.shaik@outlook.com>
2023-03-18 04:58:31 +0530

    Regex - User Registration Problem

---
+++ UserRegistrationProblems/Pattern.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblems
+{
+    public class Pattern
+    {
+        //UC1-First name starts with Cap and has minimum 3 characters
+        public static string firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+        /// <summary>
+        /// This method used to check Validation
+        /// </summary>
+        public static void ValidatePattern(string[] inputs, string pattern)
+        {
+            foreach (string result in inputs)
+            {
+                if (Regex.IsMatch(result, pattern))
+                {
+                    Console.WriteLine("{0}    => valid", result);
+                }
+                else
+                {
+                    Console.WriteLine("{0}    => Invalid", result);
+                }
+            }
+        }
+    }
+}
---
+++ UserRegistrationProblems/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = true;
+            while (check)
+            {
+                Console.Clear();
+                Console.WriteLine("Please choose any options");
+                Console.WriteLine("1.Check Validation for First Name\n" +
+                    "2.Exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        string code = Pattern.firstNamePattern;
+                        Console.WriteLine("Enter how many first name want to check for validation:");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        string[] firstNameInputs = new string[number];
+                        Console.WriteLine("\nList of FirstName between 1 to {0} for check validation:\n----------------------------------------------------", number);
+                        for (int i = 0; i < number; i++)
+                        {
+                            Console.Write("{0} =>  ", i + 1);
+                            firstNameInputs[i] = Console.ReadLine();
+                        }
+                        Console.WriteLine("\nList of valid and Invalid FirstName:\n-------------------------------------");
+                        Pattern.ValidatePattern(firstNameInputs, code);
+                        Console.Write("\nPress any key to continue...... ");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        check = false;
+                        break;
+                }
+            }
+        }
+    }
+}
